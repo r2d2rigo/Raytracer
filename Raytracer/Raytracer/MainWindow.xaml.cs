7e764f9b6aea3172126e91a9b711d5ce43386e7a
Rodrigo Diaz <r2d2rigo@gmail.com>
2017-12-21 03:06:00 +0100

    App now shows blank framebuffer.

--- Raytracer/Raytracer/MainWindow.xaml.cs
+++ Raytracer/Raytracer/MainWindow.xaml.cs
@@ -1,17 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
+﻿using System.Windows;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Raytracer
 {
@@ -20,9 +9,15 @@ namespace Raytracer
     /// </summary>
     public partial class MainWindow : Window
     {
+        private WriteableBitmap _framebuffer;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            _framebuffer = new WriteableBitmap(640, 480, 96.0, 96.0, PixelFormats.Bgra32, null);
+            _framebuffer.Clear(Colors.Black);
+            _target.Source = _framebuffer;
         }
     }
 }
