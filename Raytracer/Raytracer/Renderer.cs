4dea5d5f8be233dbd87ba0851f3527145e5bf3c7
Rodrigo Diaz <r2d2rigo@gmail.com>
2017-12-21 04:41:41 +0100

    Renderer now takes into account (hard-coded) camera origin, orientation and FOV.

--- Raytracer/Raytracer/Renderer.cs
+++ Raytracer/Raytracer/Renderer.cs
@@ -1,4 +1,6 @@
 ﻿using Raytracer.Graphics;
+using System;
+using System.Numerics;
 using System.Windows.Media;
 
 namespace Raytracer
@@ -9,11 +11,29 @@ namespace Raytracer
         {
             target.Clear(Colors.Black);
 
+            var targetAspectRatio = target.Width / (float)target.Height;
+            var fovTangent = (float)Math.Tan(Math.PI / 4.0);
+
             for (int y = 0; y < target.Height; y++)
             {
                 for (int x = 0; x < target.Width; x++)
                 {
-                    if (y > target.Height / 2)
+                    var pixelDeviceCoordinates = new Vector2(
+                        (x + 0.5f) / target.Width, 
+                        (y + 0.5f) / target.Height);
+                    var pixelScreenCoordinates = new Vector2(
+                        (2.0f * pixelDeviceCoordinates.X) - 1.0f,  
+                        1.0f - (2.0f * pixelDeviceCoordinates.Y));
+                    var pixelCameraCoordinates = new Vector3(
+                        pixelScreenCoordinates.X * targetAspectRatio * fovTangent,
+                        pixelScreenCoordinates.Y * fovTangent,
+                        -1.0f);
+
+                    var cameraToWorld = Matrix4x4.CreateLookAt(Vector3.Zero, -Vector3.UnitZ, Vector3.UnitY);
+                    var pixelRayDirection = Vector3.Normalize(pixelCameraCoordinates - Vector3.Zero);
+                    pixelRayDirection = Vector3.Transform(pixelRayDirection, cameraToWorld);
+
+                    if (pixelRayDirection.Y < 0)
                     {
                         target.SetPixel(x, y, Colors.Gray);
                     }
